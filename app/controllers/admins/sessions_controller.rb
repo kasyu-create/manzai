a917d39f49e86d6c17d3b3c125ace0c8dad13527
kasyu-create <vagrant@localhost.localdomain>
2020-07-11 12:14:04 +0000

    routing view導入

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -18,6 +18,11 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def destroy
+    reset_session
+    redirect_to new_admin_session_path
+  end
+
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,33 @@
 Rails.application.routes.draw do
 
+  # admin側ルーティング
   devise_for :admins, controllers: {
    sessions:      'admins/sessions',
    passwords:     'admins/passwords',
    registrations: 'admins/registrations'
 }
+
+  namespace :admins do
+   root :to => 'top#top'
+   resources :users, only: [:index, :show, :edit, :update]
+   resources :genres, only: [:index, :create, :edit, :update]
+   resources :boke_tukkomis, only: [:show, :index, :new, :create, :edit, :update]
+  end
+
+
+  # customer側ルーティング
   devise_for :users, controllers: {
    sessions:      'users/sessions',
    passwords:     'users/passwords',
    registrations: 'users/registrations'
 }
+  namespace :users do
+   patch 'users/withdraw' => 'users#withdraw', as: 'users_withdraw'
+   get 'show' => 'users#show'
+   get 'users/edit' => 'users#edit'
+   patch 'update' => 'users#update'
+   get 'quit' => 'users#quit'
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
